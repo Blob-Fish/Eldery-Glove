cc9694720070ee9d23c9ac42386bb722789a86d8
Blob-Fish <42638536+Blob-Fish@users.noreply.github.com>
2018-08-23 22:45:21 +1000

    Working for analogue servos

--- motortesting/motortesting.ino
+++ motortesting/motortesting.ino
@@ -1,17 +1,41 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo servoA; // create servo object to control a servo
 
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+int potpin = 0; // analog pin used to connect the potentiometer
+int val; // variable to read the value from the analog pin
+int lastval = 90;
+int servoAPosition;
+int servoBPosition;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+    servoA.attach(9); 
+    Serial.begin(9600);
+    delay(1000);
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+    val = analogRead(potpin);
+    int servpos = servoA.read();
+    Serial.println(servpos);    
+    if(val > lastval + 20){
+        Serial.print("LastVal = ");
+        Serial.println(lastval);
+        Serial.print("val = ");
+        Serial.println(val);  
+        Serial.println("Open");
+        servoA.writeMicroseconds(700);
+        delay(1000);    
+    }
+    if(val < lastval - 20){
+        Serial.print("LastVal = ");
+        Serial.println(lastval);
+        Serial.print("val = ");
+        Serial.println(val);  
+        Serial.println("Close");
+        servoA.writeMicroseconds(2300);
+        delay(1000);    
+    }
+    lastval = val;    
+    delay(50);
 }
